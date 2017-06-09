dbec21045a4b12637aa7ad3497e51578b1a5a785
ice <meltedise@gmail.com>
2017-06-09 16:29:50 +0900

    delegate where drawing contacts into Contacts.js component

--- contact-app-01/src/App.js
+++ contact-app-01/src/App.js
@@ -1,69 +1,9 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Contacts from './Contacts'
 
 class App extends Component {
-  state = {
-    contacts: [
-      {
-        "id": 1,
-        "name": "Jack",
-        "phone_number": "+81 00 0000 0001",
-        "email": "jack@example.com",
-      },
-      {
-        "id": 2,
-        "name": "Sarry",
-        "phone_number": "+81 00 0000 0002",
-        "email": "sarry@example.com",
-      },
-      {
-        "id": 3,
-        "name": "Zero",
-        "phone_number": "+81 00 0000 0000",
-        "email": "zero@example.com",
-      },
-      {
-        "id": 4,
-        "name": "Oogie Boogie",
-        "phone_number": "+81 00 0000 0004",
-        "email": "oogie-boogie@example.com",
-      },
-    ],
-  }
-
-  renderContact(contact) {
-    return (
-      <tr>
-        <td>{contact.id}</td>
-        <td>{contact.name}</td>
-        <td>{contact.phone_number}</td>
-        <td>{contact.email}</td>
-      </tr>
-    )
-  }
-
-  renderContacts() {
-    const contacts = this.state.contacts
-    const contactsTags = contacts.map((contact) => this.renderContact(contact))
-
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Phone number</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contactsTags}
-        </tbody>
-      </table>
-    )
-  }
-
   render() {
     return (
       <div className="App">
@@ -74,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {this.renderContacts()}
+        <Contacts />
       </div>
     )
   }
---
+++ contact-app-01/src/Contacts.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        "id": 1,
+        "name": "Jack",
+        "phone_number": "+81 00 0000 0001",
+        "email": "jack@example.com",
+      },
+      {
+        "id": 2,
+        "name": "Sarry",
+        "phone_number": "+81 00 0000 0002",
+        "email": "sarry@example.com",
+      },
+      {
+        "id": 3,
+        "name": "Zero",
+        "phone_number": "+81 00 0000 0000",
+        "email": "zero@example.com",
+      },
+      {
+        "id": 4,
+        "name": "Oogie Boogie",
+        "phone_number": "+81 00 0000 0004",
+        "email": "oogie-boogie@example.com",
+      },
+    ],
+  }
+
+  renderContact(contact) {
+    return (
+      <tr>
+        <td>{contact.id}</td>
+        <td>{contact.name}</td>
+        <td>{contact.phone_number}</td>
+        <td>{contact.email}</td>
+      </tr>
+    )
+  }
+
+  render() {
+    const contacts = this.state.contacts
+    const contactsTags = contacts.map((contact) => this.renderContact(contact))
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Phone number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactsTags}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export default Contacts
