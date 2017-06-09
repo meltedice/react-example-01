cf2aceba85b8e66c515036cb0d6b41aa32c14a3f
ice <meltedise@gmail.com>
2017-06-09 16:34:09 +0900

    delegate contacts data to App.js and pass data via props

--- contact-app-01/src/App.js
+++ contact-app-01/src/App.js
@@ -4,7 +4,38 @@ import './App.css'
 import Contacts from './Contacts'
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": 1,
+        "name": "Jack",
+        "phone_number": "+81 00 0000 0001",
+        "email": "jack@example.com",
+      },
+      {
+        "id": 2,
+        "name": "Sarry",
+        "phone_number": "+81 00 0000 0002",
+        "email": "sarry@example.com",
+      },
+      {
+        "id": 3,
+        "name": "Zero",
+        "phone_number": "+81 00 0000 0000",
+        "email": "zero@example.com",
+      },
+      {
+        "id": 4,
+        "name": "Oogie Boogie",
+        "phone_number": "+81 00 0000 0004",
+        "email": "oogie-boogie@example.com",
+      },
+    ],
+  }
+
   render() {
+    const contacts = this.state.contacts
+
     return (
       <div className="App">
         <div className="App-header">
@@ -14,7 +45,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Contacts />
+        <Contacts contacts={contacts}/>
       </div>
     )
   }
--- contact-app-01/src/Contacts.js
+++ contact-app-01/src/Contacts.js
@@ -1,35 +1,6 @@
 import React, { Component } from 'react'
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        "id": 1,
-        "name": "Jack",
-        "phone_number": "+81 00 0000 0001",
-        "email": "jack@example.com",
-      },
-      {
-        "id": 2,
-        "name": "Sarry",
-        "phone_number": "+81 00 0000 0002",
-        "email": "sarry@example.com",
-      },
-      {
-        "id": 3,
-        "name": "Zero",
-        "phone_number": "+81 00 0000 0000",
-        "email": "zero@example.com",
-      },
-      {
-        "id": 4,
-        "name": "Oogie Boogie",
-        "phone_number": "+81 00 0000 0004",
-        "email": "oogie-boogie@example.com",
-      },
-    ],
-  }
-
   renderContact(contact) {
     return (
       <tr>
@@ -42,7 +13,7 @@ class Contacts extends Component {
   }
 
   render() {
-    const contacts = this.state.contacts
+    const contacts = this.props.contacts
     const contactsTags = contacts.map((contact) => this.renderContact(contact))
 
     return (
