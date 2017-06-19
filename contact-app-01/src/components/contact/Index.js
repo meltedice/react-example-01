f5a481007495e895f2ccd81102d72ee83ab2f5b2
ice <meltedise@gmail.com>
2017-06-19 15:59:34 +0900

    add checkboxes for delete button

--- contact-app-01/src/components/contact/Index.js
+++ contact-app-01/src/components/contact/Index.js
@@ -3,10 +3,46 @@ import {
   Link
 } from 'react-router-dom'
 
+class Checkbox extends Component {
+  toggleCheckbox = () => {
+    const {id, handleCheckboxChange} = this.props
+    handleCheckboxChange(id)
+  }
+
+  render() {
+    const {id, checked} = this.props
+    return (
+      <input key={id} type="checkbox" onChange={this.toggleCheckbox} checked={checked}/>
+    )
+  }
+}
+
 class Index extends Component {
-  renderContact(contact) {
+  state = {
+    contactCheckedStates: new Set(),
+  }
+
+  constructor(props) {
+    super(props)
+  }
+
+  renderTableHeader = () => {
+    return (
+      <tr>
+        <th>{/* <input type="checkbox" />FIXME */}</th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Phone number</th>
+        <th>Email</th>
+        <th>Actions</th>
+      </tr>
+    )
+  }
+
+  renderContact = (contact, checked) => {
     return (
       <tr key={contact.id}>
+        <td><Checkbox id={contact.id} handleCheckboxChange={this.handleCheckboxChange} checked={checked} /></td>
         <td>{contact.id}</td>
         <td>{contact.name}</td>
         <td>{contact.phone_number}</td>
@@ -16,26 +52,45 @@ class Index extends Component {
     )
   }
 
-  render() {
+  createContactTBody = () => {
     const {contacts} = this.props
-    const contactsTags = contacts.map((contact) => this.renderContact(contact))
+    const {contactCheckedStates} = this.state
+    return contacts.map((contact) => this.renderContact(contact, contactCheckedStates.has(contact.id)))
+  }
 
+  handleCheckboxChange = (id) => {
+    const {contactCheckedStates} = this.state
+    
+    if (contactCheckedStates.has(id)) {
+      contactCheckedStates.delete(id)
+    }
+    else {
+      contactCheckedStates.add(id)
+    }
+    this.setState({contactCheckedStates: contactCheckedStates})
+  }
+
+  handleSubmit = (event) => {
+    const {contactCheckedStates} = this.state
+    event.preventDefault()
+    console.log(contactCheckedStates)
+    // TODO: delete checked items
+  }
+
+  render() {
     return (
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Phone number</th>
-              <th>Email</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {contactsTags}
-          </tbody>
-        </table>
+        <form onSubmit={this.handleSubmit}>
+          <table>
+            <thead>
+              {this.renderTableHeader()}
+            </thead>
+            <tbody>
+              {this.createContactTBody()}
+            </tbody>
+          </table>
+          <button type='submit'>Delete</button>
+        </form>
       </div>
     )
   }
